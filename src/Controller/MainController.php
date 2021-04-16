76d792eee9ecc8a23d849217bbb8593b05024484
Vengelis <gaby17250@gmail.com>
2021-04-16 19:26:01 +0200

    Rename main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TestController extends AbstractController
+class MainController extends AbstractController
 {
     #[Route('/test', name: 'test')]
     public function index(): Response
     {
         return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+            'controller_name' => 'MainController',
         ]);
     }
 

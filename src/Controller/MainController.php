e7be02f472326d1e7410a9918df1ceec415685a7
Vengelis <gaby17250@gmail.com>
2021-04-26 19:50:02 +0200

    Back pour romain

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use mysql_xdevapi\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,22 +31,42 @@ class MainController extends AbstractController
         ]);
     }
 
-    #[Route('/ressources/view/{route}/{subRouting}', name: 'indexView')]
-    public function indexView($route = null, $subRouting = null): Response
+    #[Route('/ressources/send/{route}', name: 'sendRessources')]
+    public function sendRessources($route = null): Response
+    {
+        if(!isset($route)){
+            return $this->render('ressources/send/manager/nothing.html.twig', [
+                'page_name' => "Selection du mode d'intégration"
+            ]);
+        } else {
+            return $this->render('ressources/send/manager/'.$route.'.html.twig', [
+                'page_name' => "Envoyer une ressource via ".$route
+            ]);
+        }
+
+    }
+
+    #[Route('/ressources/view/{route}/{subRouting}/{subModule}', name: 'indexView')]
+    public function indexView($route = null, $subRouting = null, $subModule = null): Response
     {
         if(isset($route) && !isset($subRouting)){
             return $this->render('ressources/view/overview.html.twig', [
                 'page_name' => $route
             ]);
-        } elseif(isset($route) && isset($subRouting)){
-            return $this->render('ressources/view/'.$subRouting.'.html.twig', [
-                'page_name' => $route." ".$subRouting
-            ]);
+        }elseif(isset($route) && isset($subRouting)){
+            if(!isset($subModule)){
+                return $this->render('ressources/view/'.$subRouting.'.html.twig', [
+                    'page_name' => $route." ".$subRouting
+                ]);
+            } elseif($subRouting == "support") {
+                return $this->render('ressources/view/support/view.html.twig', [
+                    'page_name' => $route . " " . $subModule,
+                    'ressource_id' => $subModule
+                ]);
+            }
         } else{
             return new RedirectResponse("/ressources");
         }
-
     }
-
 
 }
